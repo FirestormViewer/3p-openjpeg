24a00fc080112eba78b7e235f506a199daf25041
Ansariel <none@none>
2018-01-27 02:49:52 +0100

    Switch to Autobuild 1.1

--- build-cmd.sh
+++ build-cmd.sh
@@ -1,51 +1,52 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd "$(dirname "$0")"
 
 # turn on verbose debugging output for parabuild logs.
-set -x
+exec 4>&1; export BASH_XTRACEFD=4; set -x
 # make errors fatal
 set -e
+# complain about unset env variables
+set -u
 
 OPENJPEG_VERSION="1.4"
 OPENJPEG_SOURCE_DIR="openjpeg_v1_4_sources_r697"
 
 if [ -z "$AUTOBUILD" ] ; then 
-    fail
+    exit 1
 fi
 
 if [ "$OSTYPE" = "cygwin" ] ; then
-    export AUTOBUILD="$(cygpath -u $AUTOBUILD)"
+    autobuild="$(cygpath -u $AUTOBUILD)"
+else
+    autobuild="$AUTOBUILD"
 fi
 
-# load autobuild provided shell functions and variables
-set +x
-eval "$("$AUTOBUILD" source_environment)"
-set -x
-
 stage="$(pwd)/stage"
+
+# load autobuild provided shell functions and variables
+source_environment_tempfile="$stage/source_environment.sh"
+"$autobuild" source_environment > "$source_environment_tempfile"
+. "$source_environment_tempfile"
 
 build=${AUTOBUILD_BUILD_ID:=0}
 echo "${OPENJPEG_VERSION}.${build}" > "${stage}/VERSION.txt"
 
 pushd "$OPENJPEG_SOURCE_DIR"
     case "$AUTOBUILD_PLATFORM" in
-        "windows")
+        windows*)
             load_vsvars
 
-			if [ "${ND_AUTOBUILD_ARCH}" == "x64" ]
-			then
-				cmake . -G"Visual Studio 12 Win64" -DCMAKE_INSTALL_PREFIX=$stage -DND_WIN64_BUILD=On
+            if [ "${AUTOBUILD_WIN_VSPLATFORM}" = "Win32" ] ; then
+                cmake . -G"Visual Studio 12" -DCMAKE_INSTALL_PREFIX=$stage
+            else
+                cmake . -G"Visual Studio 12 Win64" -DCMAKE_INSTALL_PREFIX=$stage -DND_WIN64_BUILD=On
+            fi
 
-				build_sln "OPENJPEG.sln" "Release|x64"
-				build_sln "OPENJPEG.sln" "Debug|x64"
-			else
-				cmake . -G"Visual Studio 12" -DCMAKE_INSTALL_PREFIX=$stage
+            build_sln "OPENJPEG.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM"
+            build_sln "OPENJPEG.sln" "Debug|$AUTOBUILD_WIN_VSPLATFORM"
 
-				build_sln "OPENJPEG.sln" "Release|Win32"
-				build_sln "OPENJPEG.sln" "Debug|Win32"
-			fi
-
+            
             mkdir -p "$stage/lib/debug"
             mkdir -p "$stage/lib/release"
             cp bin/Release/openjpeg{.dll,.lib} "$stage/lib/release"
@@ -55,26 +56,33 @@ pushd "$OPENJPEG_SOURCE_DIR"
             mkdir -p "$stage/include/openjpeg"
             cp libopenjpeg/openjpeg.h "$stage/include/openjpeg"
         ;;
-        "darwin")
-	    cmake . -GXcode -D'CMAKE_OSX_ARCHITECTURES:STRING=i386;x86_64' \
-	    	-D'BUILD_SHARED_LIBS:bool=off' -D'BUILD_CODEC:bool=off' \
-	    	-DCMAKE_INSTALL_PREFIX=$stage -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
-	    	-DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
-	    xcodebuild -configuration Release -target openjpeg -project openjpeg.xcodeproj
-	    xcodebuild -configuration Release -target install -project openjpeg.xcodeproj
-            mkdir -p "$stage/lib/release"
-	    cp "$stage/lib/libopenjpeg.a" "$stage/lib/release/libopenjpeg.a"
-            mkdir -p "$stage/include/openjpeg"
-	    cp "$stage/include/openjpeg-$OPENJPEG_VERSION/openjpeg.h" "$stage/include/openjpeg"
-	  
+
+        darwin*)
+            cmake . -GXcode -D'CMAKE_OSX_ARCHITECTURES:STRING=i386;x86_64' \
+                -D'BUILD_SHARED_LIBS:bool=off' -D'BUILD_CODEC:bool=off' \
+                -DCMAKE_INSTALL_PREFIX=$stage -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
+                -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
+            xcodebuild -configuration Release -target openjpeg -project openjpeg.xcodeproj
+            xcodebuild -configuration Release -target install -project openjpeg.xcodeproj
+                mkdir -p "$stage/lib/release"
+            cp "$stage/lib/libopenjpeg.a" "$stage/lib/release/libopenjpeg.a"
+                mkdir -p "$stage/include/openjpeg"
+            cp "$stage/include/openjpeg-$OPENJPEG_VERSION/openjpeg.h" "$stage/include/openjpeg"
+
         ;;
-        "linux")
-			test -d "${stage}/include" && rm -rf "${stage}/include"
-			autoreconf -i
-            CFLAGS="${ND_AUTOBUILD_GCC_ARCH_FLAG}" CPPFLAGS="${ND_AUTOBUILD_GCC_ARCH_FLAG}" LDFLAGS="${ND_AUTOBUILD_GCC_ARCH_FLAG}" ./configure --target=i686-linux-gnu --prefix="$stage" --enable-png=no --enable-lcms1=no --enable-lcms2=no --enable-tiff=no --libdir="${stage}/lib"
+        linux*)
+            if [ ${AUTOBUILD_ADDRSIZE} = 32 ] ; then
+                gcc_flags = "-m32"
+            else
+                gcc_flags = "-m64 -fPIC"
+            fi
+
+            test -d "${stage}/include" && rm -rf "${stage}/include"
+            autoreconf -i
+            CFLAGS="$gcc_flags" CPPFLAGS="$gcc_flags" LDFLAGS="$gcc_flags" ./configure --target=i686-linux-gnu --prefix="$stage" --enable-png=no --enable-lcms1=no --enable-lcms2=no --enable-tiff=no --libdir="${stage}/lib"
             make
             make install
-			
+
             mv "$stage/include/openjpeg-$OPENJPEG_VERSION" "$stage/include/openjpeg"
 
             mv "$stage/lib" "$stage/release"
@@ -85,5 +93,3 @@ pushd "$OPENJPEG_SOURCE_DIR"
     mkdir -p "$stage/LICENSES"
     cp LICENSE "$stage/LICENSES/openjpeg.txt"
 popd
-
-pass
